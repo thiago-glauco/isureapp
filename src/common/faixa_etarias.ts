47bfbf16ac01846b61d37c175d538039e784f4f3
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-03 11:00:50 -0300

    1 file added

---
+++ src/common/faixa_etarias.ts
@@ -0,0 +1,12 @@
+export const faixaEtaria = {
+faixa1: {'min': 0, 'max': 18},
+faixa2: {'min': 19, 'max': 23},
+faixa3: {'min': 24, 'max': 28},
+faixa4: {'min': 29, 'max': 33},
+faixa5: {'min': 34, 'max': 38},
+faixa6: {'min': 29, 'max': 43},
+faixa7: {'min': 44, 'max': 48},
+faixa8: {'min': 49, 'max': 53},
+faixa9: {'min': 54, 'max': 58},
+faixa10: {'min': 59, 'max': 110},
+}
