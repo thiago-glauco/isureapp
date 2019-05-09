39e8f56b949b438eef832f157f6b3af62ab88ea0
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-09 10:30:43 -0300

    Amil DataSet inserted

---
+++ src/common/Amil.ts
@@ -0,0 +1,97 @@
+//Mock dos planos de saúde que será implantado depois no firebase.
+
+[
+  {Amil: {
+      Amil200: {
+        operadora: 'Amil',
+        isure: 'Básico',
+        enf: [
+          {'min': 0, 'max': 18, 'value': 248.38},
+          {'min': 19, 'max': 23, 'value': 310.48},
+          {'min': 24, 'max': 28, 'value': 388.10},
+          {'min': 29, 'max': 33, 'value': 426.91},
+          {'min': 34, 'max': 38, 'value': 448.26},
+          {'min': 29, 'max': 43, 'value': 493.09},
+          {'min': 44, 'max': 48, 'value': 616.36},
+          {'min': 49, 'max': 53, 'value': 678.00},
+          {'min': 54, 'max': 58, 'value': 847.50},
+          {'min': 59, 'max': 110, 'value': 1483.13},
+        ],
+        apto: [
+          {'min': 0, 'max': 18, 'value': 267.66},
+          {'min': 19, 'max': 23, 'value': 334.58},
+          {'min': 24, 'max': 28, 'value': 418.23},
+          {'min': 29, 'max': 33, 'value': 460.05},
+          {'min': 34, 'max': 38, 'value': 483.05},
+          {'min': 29, 'max': 43, 'value': 531.36},
+          {'min': 44, 'max': 48, 'value': 664.20},
+          {'min': 49, 'max': 53, 'value': 730.62},
+          {'min': 54, 'max': 58, 'value': 913.28},
+          {'min': 59, 'max': 110, 'value': 1598.24},
+        ]
+      },
+      Amil400: {
+        operadora: 'Amil',
+        isure: 'Básico',
+        enf: [
+          {'min': 0, 'max': 18, 'value': 291.59},
+          {'min': 19, 'max': 23, 'value': 364.49},
+          {'min': 24, 'max': 28, 'value': 455.61},
+          {'min': 29, 'max': 33, 'value': 501.17},
+          {'min': 34, 'max': 38, 'value': 526.23},
+          {'min': 29, 'max': 43, 'value': 578.85},
+          {'min': 44, 'max': 48, 'value': 723.56},
+          {'min': 49, 'max': 53, 'value': 795.92},
+          {'min': 54, 'max': 58, 'value': 994.90},
+          {'min': 59, 'max': 110, 'value': 1741.08},
+        ],
+        apto: [
+          {'min': 0, 'max': 18, 'value': 323.52},
+          {'min': 19, 'max': 23, 'value': 404.40},
+          {'min': 24, 'max': 28, 'value': 505.50},
+          {'min': 29, 'max': 33, 'value': 556.05},
+          {'min': 34, 'max': 38, 'value': 583.85},
+          {'min': 29, 'max': 43, 'value': 642.24},
+          {'min': 44, 'max': 48, 'value': 802.80},
+          {'min': 49, 'max': 53, 'value': 883.08},
+          {'min': 54, 'max': 58, 'value': 1103.85},
+          {'min': 59, 'max': 110, 'value': 1931.74},
+        ]
+      },
+      Amil500: {
+        operadora: 'Amil',
+        isure: 'Básico',
+        apto: [
+          {'min': 0, 'max': 18, 'value': 377.84},
+          {'min': 19, 'max': 23, 'value': 472.30},
+          {'min': 24, 'max': 28, 'value': 590.38},
+          {'min': 29, 'max': 33, 'value': 649.42},
+          {'min': 34, 'max': 38, 'value': 681.89},
+          {'min': 29, 'max': 43, 'value': 750.08},
+          {'min': 44, 'max': 48, 'value': 937.60},
+          {'min': 49, 'max': 53, 'value': 1031.36},
+          {'min': 54, 'max': 58, 'value': 1289.20},
+          {'min': 59, 'max': 110, 'value': 2256.10},
+        ],
+      },
+      Amil700: {
+        apto: [
+          {'min': 0, 'max': 18, 'value': 437.19},
+          {'min': 19, 'max': 23, 'value': 546.49},
+          {'min': 24, 'max': 28, 'value': 683.11},
+          {'min': 29, 'max': 33, 'value': 751.42},
+          {'min': 34, 'max': 38, 'value': 788.99},
+          {'min': 29, 'max': 43, 'value': 867.89},
+          {'min': 44, 'max': 48, 'value': 1084.86},
+          {'min': 49, 'max': 53, 'value': 1193.35},
+          {'min': 54, 'max': 58, 'value': 1491.69},
+          {'min': 59, 'max': 110, 'value': 2610.46},
+        ],
+      }
+    }
+  },
+  {Bradesco: {
+
+  }
+  }
+]
