7d21188f9a98653975eca1fb88320fb0cc70ece2
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-24 16:25:07 -0300

    mail validation

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { User } from '../common/user';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
