57df25c8a34317b3641bceab8de20dc4782be050
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-25 12:23:56 -0300

    createUserData

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: false,
   firebase: {
-  apiKey: "AIzaSyCxk7OYhCnn06Rb0kt4_Zu59HSapOyd7_I",
-  authDomain: "isure-3f25e.firebaseapp.com",
-  databaseURL: "https://isure-3f25e.firebaseio.com",
-  projectId: "isure-3f25e",
-  storageBucket: "isure-3f25e.appspot.com",
-  messagingSenderId: "928411650252",
-  appId: "1:928411650252:web:f0909596170afbac"
+  apiKey: "AIzaSyBkC2BtT7WI9jWUppiGtN9SW5mfHIS2m_0",
+  authDomain: "isureapp-17148.firebaseapp.com",
+  databaseURL: "https://isureapp-17148.firebaseio.com",
+  projectId: "isureapp-17148",
+  storageBucket: "isureapp-17148.appspot.com",
+  messagingSenderId: "913471816189",
+  appId: "1:913471816189:web:62f59cebeb715fb5"
   }
 };
