7742be10428ddbc2551c92ec2b532aaff40bb304
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-27 10:40:31 -0300

    2 files modified

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -9,18 +9,27 @@ import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   formRegister = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100), Validators.pattern('[a-zA-Z ]*')]],
-    cpf: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('[0-9]*')]],
-    cel: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('[0-9 ]*')]],
+    cpf: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('[0-9 ]*')]],
+    cel: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15), Validators.pattern('[0-9 ]*')]],
     email: ['', [Validators.required, Validators.minLength(4)]],
     passwd: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
     confirm_passwd: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
   });
+
+  errors = {
+    passwd: false,
+  }
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
+    if( this.formRegister.controls.passwd !== this.formRegister.controls.confirm_passwd) {
+      this.errors.passwd = true
+    } else {
+      this.errors.passwd = false
+    }
     console.dir(this.formRegister.value);
     console.dir(this.formRegister.controls.name.errors);
   }
