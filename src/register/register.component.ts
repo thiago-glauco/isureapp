6ed946360a2ba6e6ab1dde5b2ddfa137474b84f5
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-27 10:19:46 -0300

    register validation2

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -7,17 +7,22 @@ import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  private formRegister: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-    cpf: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11),Validators.pattern('^[0-9]*')]],
-    cel: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(11),Validators.pattern('^[0-9]*')]],
-    email: ['', [Validators.required, , Validators.minLength(3), Validators.email]],
-    passwd: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
-    confirm_passwd: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+  formRegister = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100), Validators.pattern('[a-zA-Z ]*')]],
+    cpf: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('[0-9]*')]],
+    cel: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('[0-9 ]*')]],
+    email: ['', [Validators.required, Validators.minLength(4)]],
+    passwd: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
+    confirm_passwd: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
   });
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.dir(this.formRegister.value);
+    console.dir(this.formRegister.controls.name.errors);
   }
 
 }
