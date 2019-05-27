526391a36ecda58f0d5e613e0e6fc4593ee4c1f8
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-27 09:09:48 -0300

    register validation

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -8,12 +8,12 @@ import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   private formRegister: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    cpf: ['', Validators.required],
-    cel: ['', Validators.required],
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+    cpf: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11),Validators.pattern('^[0-9]*')]],
+    cel: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(11),Validators.pattern('^[0-9]*')]],
     email: ['', [Validators.required, , Validators.minLength(3), Validators.email]],
-    passwd: ['', Validators.required],
-    confirm_passwd: ['', Validators.required],
+    passwd: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+    confirm_passwd: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
   });
   constructor(private fb: FormBuilder) { }
 
