dfbf96ab8086e474f5a6bc5a57c404925cdd63b9
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-06-22 12:16:35 -0300

    2 files modified

--- src/common/isurePlans.ts
+++ src/common/isurePlans.ts
@@ -3,20 +3,20 @@ import { ISurePlan } from './isurePlan';
 export const isurePlans: ISurePlan[] = [
   {
     "id": "0",
-    "name": "iSure Básico",
-    "description": "Planos de baixo custo",
+    "name": "Essencial",
+    "description": "Os planos do tipo Essencial fornecem todo o tipo de assitência médica e hospitalar que você pode precisar, dentro dos termos legais do Brasil. Estes planos possuem um preço mais acessível pois contam com uma rede de atendimento mais enxuta, sem deixar de realizar todos os procedimentos médicos previstos pela ANS. As internações realizadas em enfermarias.",
     "image": "https://firebasestorage.googleapis.com/v0/b/isureapp-17148.appspot.com/o/AppImages%2Fessencial.jpg?alt=media&token=a7df8b87-aec4-4d3b-abcb-c98e57d2693d"
   },
   {
     "id": "1",
-    "name": "iSure Intermediário",
-    "description": "Planos de custo intermediário",
+    "name": "Conforto",
+    "description": "Para quem precisa de atendimento médico e não abre mão do conforto de um quarto nos casos de internação ou de uma ampla rede de atendimento. Os planos do tipo Conforto fornecem todo o tipo de assitência médica e hospitalar que você pode precisar, dentro dos termos legais do Brasil. Estes planos possuem excelente relação de custo x benefício, pois permitem que você acesse  todos os procedimentos médicos previstos pela ANS em uma rede de atendimento ampla e de fácil acesso. As internações são realizadas em quartos.",
     "image": "https://firebasestorage.googleapis.com/v0/b/isureapp-17148.appspot.com/o/AppImages%2Fconforto.jpg?alt=media&token=f86c1a30-24ae-4b1a-8e19-81a3f4a1d90f"
   },
   {
     "id": "2",
-    "name": "iSure Executivo",
-    "description": "Planos destinados a empresários e executivos",
+    "name": "Executivo",
+    "description": "A ISure entende perfeitamente as necessidades de mobilidade, tempo e a necessidade de conforto e privacidade que a vida impõem a empresários e executivos. Por isso, temos aqui planos com redes de atendimento em todo Brasil e internações realizadas em quartos, para que você possa realizar suas viagens profissionais ou de lazer com a tranquilidade de quem sabe que está .Planos destinados a empresários e executivos",
     "image": "https://firebasestorage.googleapis.com/v0/b/isureapp-17148.appspot.com/o/AppImages%2Fexecutivo.jpg?alt=media&token=44d7b2e8-8b3e-4539-9514-2d07b078e081"
   }
 ]
