80e219e588f20a8c0b0df4fb8bceee665cee8d5c
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-07-23 17:03:05 -0300

    2 files modified

--- src/app/calculation-card/calculation-card.component.ts
+++ src/app/calculation-card/calculation-card.component.ts
@@ -17,6 +17,10 @@ import { Observable } from 'rxjs';
 export class CalculationCardComponent implements OnInit {
 
   selectedPlans: Plano[];
+  calculationData: any[];
+  result: Number;
+  selectedOperator;
+  finalData: any[] = [];
 
   constructor(
       private location: Location,
@@ -27,66 +31,133 @@ export class CalculationCardComponent implements OnInit {
     }
 
   ngOnInit( ) {
-    this.operatorsPlansService.getPlansByCategoryAndoperator("Conf","Amil")
+    this.calculationData = this.calculationService.getResult();
+    this.operatorsPlansService.getPlansByCategoryAndoperator(this.calculationData.isurePlan,this.calculationData.operadora)
     .subscribe(
-      (result) => {console.log("the result is"); console.dir(result)}
-    )
+      (result) => {
+        console.log("the result is"); console.dir(result);
+for( let plan of result) {
+      console.log(plan);
+      let planValue = 0;
+      for(let life of this.calculationData.lifes) {
+        if( life.age || life.age === 0) {
+          if ( life.age >=  plan.lifetable[0].min && life.age <=  plan.lifetable[0].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[0])
+            planValue += plan.lifetable[0].value;
+          }
+          else if ( life.age >=  plan.lifetable[1].min && life.age <=  plan.lifetable[1].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[1])
+            planValue += plan.lifetable[1].value;
+          }
+          else if ( life.age >=  plan.lifetable[2].min && life.age <=  plan.lifetable[2].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[2])
+            planValue += plan.lifetable[2].value;
+          }
+          else if ( life.age >=  plan.lifetable[3].min && life.age <=  plan.lifetable[3].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[3])
+            planValue += plan.lifetable[3].value;
+          }
+          else if ( life.age >=  plan.lifetable[4].min && life.age <=  plan.lifetable[4].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[4])
+            planValue += plan.lifetable[4].value;
+          }
+          else if ( life.age >=  plan.lifetable[5].min && life.age <=  plan.lifetable[5].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[5])
+            planValue += plan.lifetable[5].value;
+          }
+          else if ( life.age >=  plan.lifetable[6].min && life.age <=  plan.lifetable[6].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[6])
+            planValue += plan.lifetable[6].value;
+          }
+          else if ( life.age >=  plan.lifetable[7].min && life.age <=  plan.lifetable[7].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[7])
+            planValue += plan.lifetable[7].value;
+          }
+          else if ( life.age >=  plan.lifetable[8].min && life.age <=  plan.lifetable[8].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[8])
+            planValue += plan.lifetable[8].value;
+          }
+          else if ( life.age >=  plan.lifetable[9].min && life.age <=  plan.lifetable[9].max) {
+            console.log(life.age);
+            console.log(plan.lifetable[9])
+            planValue += plan.lifetable[9].value;
+          }
+          else {
+            console.log("Error: out of range");
+          }                                                                
+        }
+      }
+      this.finalData.push({ plan: plan.name, valor: planValue });
+    }
       
+  });
   }
+
+
+    
 
   
     /*
     for(let life of this.calculationData.lifes) {
       if( life.age || life.age === 0) {
-        if ( life.age >=  this.selectedOperator.lifeTable[0].min && life.age <=  this.selectedOperator.lifeTable[0].max) {
+        if ( life.age >=  this.selectedOperator.lifetable[0].min && life.age <=  this.selectedOperator.lifetable[0].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[0])
-          this.finalValues += this.selectedOperator.lifeTable[0].value;
+          console.log(this.selectedOperator.lifetable[0])
+          this.finalValues += this.selectedOperator.lifetable[0].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[1].min && life.age <=  this.selectedOperator.lifeTable[1].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[1].min && life.age <=  this.selectedOperator.lifetable[1].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[1])
-          this.finalValues += this.selectedOperator.lifeTable[1].value;
+          console.log(this.selectedOperator.lifetable[1])
+          this.finalValues += this.selectedOperator.lifetable[1].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[2].min && life.age <=  this.selectedOperator.lifeTable[2].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[2].min && life.age <=  this.selectedOperator.lifetable[2].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[2])
-          this.finalValues += this.selectedOperator.lifeTable[2].value;
+          console.log(this.selectedOperator.lifetable[2])
+          this.finalValues += this.selectedOperator.lifetable[2].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[3].min && life.age <=  this.selectedOperator.lifeTable[3].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[3].min && life.age <=  this.selectedOperator.lifetable[3].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[3])
-          this.finalValues += this.selectedOperator.lifeTable[3].value;
+          console.log(this.selectedOperator.lifetable[3])
+          this.finalValues += this.selectedOperator.lifetable[3].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[4].min && life.age <=  this.selectedOperator.lifeTable[4].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[4].min && life.age <=  this.selectedOperator.lifetable[4].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[4])
-          this.finalValues += this.selectedOperator.lifeTable[4].value;
+          console.log(this.selectedOperator.lifetable[4])
+          this.finalValues += this.selectedOperator.lifetable[4].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[5].min && life.age <=  this.selectedOperator.lifeTable[5].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[5].min && life.age <=  this.selectedOperator.lifetable[5].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[5])
-          this.finalValues += this.selectedOperator.lifeTable[5].value;
+          console.log(this.selectedOperator.lifetable[5])
+          this.finalValues += this.selectedOperator.lifetable[5].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[6].min && life.age <=  this.selectedOperator.lifeTable[6].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[6].min && life.age <=  this.selectedOperator.lifetable[6].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[6])
-          this.finalValues += this.selectedOperator.lifeTable[6].value;
+          console.log(this.selectedOperator.lifetable[6])
+          this.finalValues += this.selectedOperator.lifetable[6].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[7].min && life.age <=  this.selectedOperator.lifeTable[7].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[7].min && life.age <=  this.selectedOperator.lifetable[7].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[7])
-          this.finalValues += this.selectedOperator.lifeTable[7].value;
+          console.log(this.selectedOperator.lifetable[7])
+          this.finalValues += this.selectedOperator.lifetable[7].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[8].min && life.age <=  this.selectedOperator.lifeTable[8].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[8].min && life.age <=  this.selectedOperator.lifetable[8].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[8])
-          this.finalValues += this.selectedOperator.lifeTable[8].value;
+          console.log(this.selectedOperator.lifetable[8])
+          this.finalValues += this.selectedOperator.lifetable[8].value;
         }
-        else if ( life.age >=  this.selectedOperator.lifeTable[9].min && life.age <=  this.selectedOperator.lifeTable[9].max) {
+        else if ( life.age >=  this.selectedOperator.lifetable[9].min && life.age <=  this.selectedOperator.lifetable[9].max) {
           console.log(life.age);
-          console.log(this.selectedOperator.lifeTable[9])
-          this.finalValues += this.selectedOperator.lifeTable[9].value;
+          console.log(this.selectedOperator.lifetable[9])
+          this.finalValues += this.selectedOperator.lifetable[9].value;
         }
         else {
           console.log("Error: out of range");
